5ece88b904890040535bb830557ba399959a3520
Ivan <ivan.silva.facebook@gmail.com>
2021-02-25 20:46:19 +0100

    feature: add airplane passengers testbench

---
+++ testbench_airplanes.py
@@ -0,0 +1,19 @@
+from generic_multiple_series_lookback import generic_multiple_series_lookback
+
+input_csv_file = "data/airline-passengers.csv"
+csv_sep = ","
+sel_features = [
+    "Passengers"
+]
+
+generic_multiple_series_lookback(
+    input_csv_file,
+    sel_features,
+    train_split=0.67,
+    look_back=1,
+    batch_size=1,
+    epochs=40,
+    plot_prefix="airline_passengers",
+    lstm_units=32,
+    ncols=1
+)
